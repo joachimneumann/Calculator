7c291708fbbfb6fb5dc9b46da5768c5a89b1dc8d
Joachim Neumann <joachim@mindo.io>
2021-09-20 07:10:03 +0700

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Joachim Neumann on 20/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
