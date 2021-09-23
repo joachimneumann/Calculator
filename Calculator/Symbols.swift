f20240b5fade19f11357bcbf8dba2c5097e9528c
Joachim Neumann <joachim@mindo.io>
2021-09-23 15:50:51 +0700

    Symbols

---
+++ Calculator/Symbols.swift
@@ -0,0 +1,21 @@
+//
+//  Symbols.swift
+//  Calculator
+//
+//  Created by Joachim Neumann on 23/09/2021.
+//
+
+import SwiftUI
+
+struct SquareRoot: Shape {
+    func path(in rect: CGRect) -> Path {
+            var path = Path()
+
+            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+
+            return path
+        }
+}
