ef2e075c94fbe413eab160a6160b1e35385b7ea9
Joachim Neumann <joachim@mindo.io>
2021-10-01 12:09:18 +0700

    corrected symbols

--- Calculator/Symbols.swift
+++ Calculator/Symbols.swift
@@ -7,21 +7,20 @@
 
 import SwiftUI
 
-struct SquareRootShape: Shape {
+struct RootShape: Shape {
     func path(in rect: CGRect) -> Path {
         var path = Path()
         let w: CGFloat = rect.size.width
         let h: CGFloat = rect.size.height
-        let s: CGFloat = min(w,h)
         let steepness: CGFloat = 2.8
         let f: CGFloat = 0.6
-        let startX: CGFloat = 0.4 * s
-        let startY: CGFloat = 0.55 * s
-        let downX: CGFloat = startX+f*0.08*s
-        let downY: CGFloat = startY+f*0.08*steepness*s
-        let upX: CGFloat = downX+f*0.2*s
-        let upY: CGFloat = downY-f*0.2*steepness*s
-        let endX: CGFloat = upX+f*0.35*s
+        let startX: CGFloat = 0.3 * w
+        let startY: CGFloat = 0.55 * h
+        let downX: CGFloat = startX + f * 0.08 * w
+        let downY: CGFloat = startY + f * 0.08 * h * steepness
+        let upX: CGFloat = downX + f * 0.2 * w
+        let upY: CGFloat = downY - f * 0.2 * h * steepness
+        let endX: CGFloat = upX + f * 0.35 * w
         let endY: CGFloat = upY
         
         path.move(to: CGPoint(x: startX, y: startY))
@@ -37,12 +36,12 @@ struct SlashShape: Shape {
         var path = Path()
         let w: CGFloat = rect.size.width
         let h: CGFloat = rect.size.height
-        let s: CGFloat = min(w,h)
-        let steepness: CGFloat = 1.5
-        let startX: CGFloat = 0.5 * s
-        let startY: CGFloat = 0.65 * s
-        let upX: CGFloat = startX+0.18*s
-        let upY: CGFloat = startY-0.18*steepness*s
+        let sw = 0.2*w
+        let steepness: CGFloat = 1.3
+        let startX: CGFloat = 0.5 * w - 0.5 * sw
+        let startY: CGFloat = 0.5 * h + 0.5 * steepness * sw
+        let upX: CGFloat = startX+sw
+        let upY: CGFloat = startY-sw*steepness
         
         path.move(to: CGPoint(x: startX, y: startY))
         path.addLine(to: CGPoint(x: upX,   y: upY))
@@ -56,21 +55,28 @@ struct Root: View {
     var body: some View {
         ZStack {
             GeometryReader { geo in
-                let s = min(geo.size.width, geo.size.height)
-                let fontSize1:CGFloat = CGFloat(0.21276) * s
-                let fontSize2:CGFloat = CGFloat(0.27659) * s
-                let offsetX1:CGFloat  = CGFloat(0.33) * s
-                let offsetY1:CGFloat  = CGFloat(0.25) * s
-                let offsetX2:CGFloat  = CGFloat(0.58) * s
-                let offsetY2:CGFloat  = CGFloat(0.37) * s
-                Text(root)
-                    .font(.system(size: fontSize1, weight: .semibold))
-                    .offset(x: offsetX1, y: offsetY1)
-                SquareRootShape()
-                    .stroke(strokeColor, style: StrokeStyle(lineWidth: 1.6*s/47, lineCap: CGLineCap.square, lineJoin: CGLineJoin.bevel))
-                Text("X")
-                    .font(.system(size: fontSize2, weight: .semibold))
-                    .offset(x: offsetX2, y: offsetY2)
+                let w = geo.size.width
+                let h = geo.size.height
+                VStack(spacing:0) {
+                    Spacer(minLength: 0)
+                    HStack(spacing:0) {
+                        ZStack {
+                            Spacer(minLength: 0)
+                            let fontSize1:CGFloat = CGFloat(0.21276) * h
+                            let fontSize2:CGFloat = CGFloat(0.27659) * h
+                            Text(root)
+                                .font(.system(size: fontSize1, weight: .semibold))
+                                .offset(x: -0.18*w, y: -0.10*h)
+                            RootShape()
+                                .stroke(strokeColor, style: StrokeStyle(lineWidth: 1.6*h/47, lineCap: CGLineCap.square, lineJoin: CGLineJoin.bevel))
+                            Text("X")
+                                .font(.system(size: fontSize2, weight: .semibold))
+                                .offset(x: 0.08*w, y: 0.05*h)
+                        }
+                        Spacer(minLength: 0)
+                    }
+                    Spacer(minLength: 0)
+                }
             }
         }
     }
@@ -85,38 +91,31 @@ struct One_x: View {
     var body: some View {
         ZStack {
             GeometryReader { geo in
-                let s = min(geo.size.width, geo.size.height)
-                Text("1")
-                    .font(.system(size: s*0.25))
-                    .offset(x: 0.4*s, y: 0.3*s)
-                SlashShape()
-                    .stroke(strokeColor, style: StrokeStyle(lineWidth: 1.6*s/47, lineCap: CGLineCap.square, lineJoin: CGLineJoin.bevel))
-                Text("x")
-                    .font(.system(size: s*0.25))
-                    .offset(x: 0.65*s, y: 0.45*s)
-            }
-        }
-        //.background(Color.yellow)
-    }
-}
-
-
-
-struct Log10: View {
-    var body: some View {
-        ZStack {
-            GeometryReader { geo in
-                let s = min(geo.size.width, geo.size.height)
-                Text("log")
-                    .font(.system(size: s*0.4))
-                    .offset(x: 0.2*s, y: 0.25*s)
-                Text("10")
-                    .font(.system(size: s*0.22))
-                    .offset(x: 0.73*s, y: 0.5*s)
+                let w = geo.size.width
+                let h = geo.size.height
+                VStack(spacing:0) {
+                    Spacer(minLength: 0)
+                    HStack(spacing:0) {
+                        Spacer(minLength: 0)
+                        ZStack {
+                            Text("1")
+                                .font(.system(size: h*0.25))
+                                .offset(x: -0.10*w, y: -0.10*h)
+                            SlashShape()
+                                .stroke(strokeColor, style: StrokeStyle(lineWidth: 1.6*w/47, lineCap: CGLineCap.square, lineJoin: CGLineJoin.bevel))
+                            Text("x")
+                                .font(.system(size: h*0.25))
+                                .offset(x: 0.10*w, y: 0.07*h)
+                        }
+                        Spacer(minLength: 0)
+                    }
+                    Spacer(minLength: 0)
+                }
             }
         }
     }
 }
+
 
 struct Logx: View {
     let base: String
@@ -124,12 +123,19 @@ struct Logx: View {
         ZStack {
             GeometryReader { geo in
                 let s = min(geo.size.width, geo.size.height)
-                Text("log")
-                    .font(.system(size: s*0.4))
-                    .offset(x: 0.25*s, y: 0.25*s)
-                Text(base)
-                    .font(.system(size: s*0.22))
-                    .offset(x: 0.78*s, y: 0.5*s)
+                VStack(spacing:0) {
+                    Spacer(minLength: 0)
+                    HStack(spacing:0) {
+                        Spacer(minLength: 0)
+                        Text("log")
+                            .font(.system(size: s*0.4))
+                        Text(base)
+                            .font(.system(size: s*0.22))
+                            .offset(x: 0.0, y: 0.13*s)
+                        Spacer(minLength: 0)
+                    }
+                    Spacer(minLength: 0)
+                }
             }
         }
     }
@@ -139,18 +145,23 @@ struct Logx: View {
 struct Pow: View {
     let base: String
     let exponent: String
-    let baseXOffset: CGFloat
-    let exponentXOffset: CGFloat
     var body: some View {
         ZStack {
             GeometryReader { geo in
                 let s = min(geo.size.width, geo.size.height)
-                Text(base)
-                    .font(.system(size: s*0.4))
-                    .offset(x: (0.4-exponentXOffset+baseXOffset)*s, y: 0.25*s)
-                Text(exponent)
-                    .font(.system(size: s*0.22))
-                    .offset(x: (0.62+exponentXOffset+baseXOffset)*s, y: 0.25*s)
+                VStack(spacing:0) {
+                    Spacer(minLength: 0)
+                    HStack(spacing:0) {
+                        Spacer(minLength: 0)
+                        Text(base)
+                            .font(.system(size: s*0.4))
+                        Text(exponent)
+                            .font(.system(size: s*0.22))
+                            .offset(x: 0.0, y: -0.13*s)
+                        Spacer(minLength: 0)
+                    }
+                    Spacer(minLength: 0)
+                }
             }
         }
     }
