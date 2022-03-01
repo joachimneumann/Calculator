ea6197bbe4259645cfd58963c54a8e229d2e8d4a
Joachim Neumann <joachim@joachimneumann.com>
2022-02-28 21:28:17 -0300

    works for non scientific numbers

--- Calculator/SmartDisplay.swift
+++ Calculator/SmartDisplay.swift
@@ -9,27 +9,28 @@ import SwiftUI
 
 struct SmartDisplay: View {
 
-    /* Indicates whether the text has been truncated in its display. */
+    /// does the number fit into one line?
     @State private var tooLongNumber: Bool = false
 
-
-    private var text: String
-
-    init(_ text: String) {
-        self.text = text
-    }
+    let text: String
+    let height: CGFloat
 
     struct DisplayScrollView: View {
+        let height: CGFloat
         let text: String
         var body: some View {
-            Text(text)
-                .multilineTextAlignment(.trailing)
+            ScrollView {
+                Text(text)
+                    .multilineTextAlignment(.trailing)
+            }
+            .frame(height: height)
         }
     }
     var body: some View {
         VStack {
             // Render the real text (which might or might not be limited)
-            DisplayScrollView(text: text)
+            Spacer(minLength: 0.0)
+            DisplayScrollView(height: 100, text: text)
                 .frame(maxWidth: .infinity, alignment: .trailing)
                 .background(tooLongNumber ? .yellow : .green).opacity(0.4)
 
@@ -52,5 +53,14 @@ struct SmartDisplay: View {
                         .hidden() // Hide the background
             )
         }
+    }
+}
+
+struct SmartDisplay_Previews: PreviewProvider {
+    static var previews: some View {
+        SmartDisplay(text: "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890", height: 200)
+//        SmartDisplay("123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890")
+//        ContentView(text: "1,23E6")
+//        ContentView(text: "1,23456789012345678901234567E6")
     }
 }
