32e265219c45a947661c1334c63435b7a66c610f
Joachim Neumann <joachim@joachimneumann.com>
2022-11-08 10:17:32 +0700

    better animation in main view

--- Calculator/MainView.swift
+++ Calculator/MainView.swift
@@ -46,62 +46,49 @@ struct MainView: View {
     }
     
     @State private var showDetails = false
-    @State var angle: Angle = .degrees(0.0)
     var body: some View {
         VStack(spacing: 0.0) {
             Spacer(minLength: 0.0)
-            if !brain.zoomed {
-                HStack(spacing: 0.0) {
+            HStack(spacing: 0.0) {
+                if !brain.zoomed {
                     SingleLineDisplay(number: brain.last, fontSize: t.displayFontSizeCandidate, length: t.digitsInDisplay)
                         .frame(height: t.displayheight, alignment: .topTrailing)
                         .frame(maxWidth: .infinity, alignment: .topTrailing)
-                    if t.isPad || !t.isPortrait {
-                        Image(systemName: "plus.circle.fill")
-                            .font(.system(size: t.iconSize, weight: .thin))
-                            .foregroundColor(t.digits_1_9.textColor)
-                            .rotationEffect(angle)
-                            .onTapGesture {
-                                withAnimation(Animation.linear(duration: 0.3)) {
-                                    angle = .degrees(-45.0)
-                                }
-                                withAnimation() {
-                                    brain.zoomed.toggle()
-                                }
-                            }
-                            .padding(.top, t.iconSize*0.1)
-                            .frame(height: t.displayheight, alignment: .topTrailing)
-                    }
-                }
-                Keys(brain: brain, t: t)
-            } else {
-                HStack(spacing: 0.0) {
+                        //.background(Color.yellow)
+                } else {
                     MultiLineDisplay(number: brain.last, fontSize: t.displayFontSizeCandidate, length: t.digitsInDisplay)
+                        .frame(height: t.displayheight + t.allkeysHeight)
+                        //.background(Color.yellow)
+                }
+                if t.isPad || !t.isPortrait {
                     VStack(spacing: 0.0) {
                         Image(systemName: "plus.circle.fill")
                             .font(.system(size: t.iconSize, weight: .thin))
                             .foregroundColor(t.digits_1_9.textColor)
-                            .rotationEffect(angle)
+                            .rotationEffect(brain.zoomed ? .degrees(-45.0) : .degrees(0.0))
                             .onTapGesture {
-                                withAnimation(Animation.linear(duration: 0.3)) {
-                                    angle = .degrees(0.0)
-                                }
                                 withAnimation() {
                                     brain.zoomed.toggle()
                                 }
                             }
                             .padding(.top, t.iconSize*0.1)
-                        Image(systemName: "switch.2")
-                            .font(.system(size: t.iconSize*0.75, weight: .thin))
-                            .padding(.top, t.iconSize*0.25)
-                            .foregroundColor(t.digits_1_9.textColor)
-                            .onTapGesture {
-                                withAnimation() {
+                        if brain.zoomed {
+                            Image(systemName: "switch.2")
+                                .font(.system(size: t.iconSize*0.75, weight: .thin))
+                                .padding(.top, t.iconSize*0.25)
+                                .foregroundColor(t.digits_1_9.textColor)
+                                .onTapGesture {
+                                    withAnimation() {
+                                    }
                                 }
-                            }
+                        }
                         Spacer(minLength: 0.0)
                     }
+                    .frame(height: t.displayheight + (brain.zoomed ? t.allkeysHeight : 0.0), alignment: .topTrailing)
                 }
-                .frame(height: t.displayheight + t.allkeysHeight)
+            }
+            if !brain.zoomed {
+                Keys(brain: brain, t: t)
             }
         }
     }
