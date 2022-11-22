c929adfa1f736e65c3f80c19f483b10d159389fe
Joachim Neumann <joachim@joachimneumann.com>
2022-11-22 10:24:14 +0700

    better button press animation

--- Calculator/KeyBackground.swift
+++ Calculator/KeyBackground.swift
@@ -37,18 +37,42 @@ fileprivate extension View {
 private struct OnTouchUpGestureModifier: ViewModifier {
     @Binding var tapped: Bool
     let callback: () -> Void
+    @State var downAnimationFinished = false
+    @State var upHasHappended = false
+    let downTime = 0.1
+    let upTime = 0.3
     
+    ///  The animation will always wait for the downanimation to finish
+    ///  This is a more clear visual feedback to the user that the button has been pressed
     func body(content: Content) -> some View {
         content
             .simultaneousGesture(DragGesture(minimumDistance: 0)
                 .onChanged { _ in
+                    self.downAnimationFinished = false
+                    upHasHappended = false
                     if !self.tapped {
-                        self.tapped = true
+                        withAnimation(.easeIn(duration: downTime)) {
+                            self.tapped = true
+                        }
+                        DispatchQueue.main.asyncAfter(deadline: .now() + downTime) {
+                            self.downAnimationFinished = true
+                            if upHasHappended {
+                                withAnimation(.easeIn(duration: upTime)) {
+                                    self.tapped = false
+                                }
+                            }
+                        }
                     }
                 }
                 .onEnded { _ in
-                    self.tapped = false
                     self.callback()
+                    if self.downAnimationFinished {
+                        withAnimation(.easeIn(duration: upTime)) {
+                            self.tapped = false
+                        }
+                    } else {
+                        upHasHappended = true
+                    }
                 })
     }
 }
