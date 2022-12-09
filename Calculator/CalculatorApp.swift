e4d46facca7c30ae7b1bcb2cbfa21593d4a0a9e1
Joachim Neumann <joachim@joachimneumann.com>
2022-12-09 20:02:10 +0700

    removing forceLandscape

--- Calculator/CalculatorApp.swift
+++ Calculator/CalculatorApp.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-    static var forceLandscape = false
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         return true
     }
