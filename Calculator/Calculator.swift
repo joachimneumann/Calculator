da581f894ecac3057ba4538a5d69079e59f52bd9
Joachim Neumann <joachim@joachimneumann.com>
2022-12-11 01:41:26 +0700

    extracting Views in Calculator

--- Calculator/Calculator.swift
+++ Calculator/Calculator.swift
@@ -26,99 +26,36 @@ struct Calculator: View {
         NavigationStack {
             Rectangle()
                 .overlay() {
-                    VStack(spacing: 0.0) {
-                        if isPortrait {
-                            PortraitDisplay(
-                                displayData: model.displayData,
-                                fullLength: model.displayData.short.count == model.lengths.withoutComma,
-                                //ePadding: model.lengths.ePadding,
-                                smallFont: Font(UIFont.monospacedDigitSystemFont(ofSize: singleLineFontSize, weight: .thin)),
-                                largeFont: Font(UIFont.monospacedDigitSystemFont(ofSize: singleLineFontSize * 1.5, weight: .thin)),
-                                fontScaleFactor: 1.5,
-                                displayWidth: model.displayWidth)
-                            //.background(Color.yellow)
-                            .padding(.bottom, keyboardSize.height)
-                            //                        .offset(y: displayYOffset)
-                        } else {
-                            HStack(spacing: 0.0) {
-                                LandscapeDisplay(
-                                    zoomed: model.zoomed,
-                                    displayData: model.displayData,
-                                    ePadding: model.lengths.ePadding,
-                                    font: Font(UIFont.monospacedDigitSystemFont(ofSize: singleLineFontSize, weight: .thin)),
-                                    isCopyingOrPasting: false,
-                                    precisionString: "Model.precision.useWords",
-                                    displayWidth: model.displayWidth)
-                                .offset(x: -displayTrailingOffset, y: displayBottomOffset)
-                                //                        .background(Color.green).opacity(0.4)
-                                .animation(Animation.easeInOut(duration: 0.4), value: model.zoomed)
-                            }
-                            //.background(Color.yellow)
-                            Spacer()
-                        }
-                    }
+                    Display(isPortrait: isPortrait,
+                            isZoomed: model.zoomed,
+                            displayData: model.displayData,
+                            lengths: model.lengths,
+                            singleLineFontSize: singleLineFontSize,
+                            displayWidth: model.displayWidth,
+                            height: keyboardSize.height,
+                            displayTrailingOffset: displayTrailingOffset,
+                            displayBottomOffset: displayBottomOffset)
                 }
             
                 .overlay() { /// Icons
-                    if !isPortrait {
-                        HStack(spacing: 0.0) {
-                            Spacer(minLength: 0.0)
-                            let size = keyboardSize.height * 0.13
-                            VStack(spacing: 0.0) {
-                                //let _ = print("model.isCalculating \(model.isCalculating)")
-                                if model.isCalculating {
-                                    AnimatedDots(color: .gray)
-                                        .padding(.top, size * 0.55)
-                                        .animation(Animation.easeInOut(duration: 0.4), value: model.zoomed)
-                                } else {
-                                    PlusKey(keyInfo: model.keyInfo["plusKey"]!, model: model, size: CGSize(width: size, height: size))
-                                        .padding(.top, size * 0.25)
-                                    //                                    .offset(y: size * 0.25)
-                                    if model.zoomed {
-                                        NavigationLink {
-                                            Settings(model: model)
-                                        } label: {
-                                            Image(systemName: "switch.2")
-                                                .resizable()
-                                                .scaledToFit()
-                                                .font(.system(size: keyboardSize.height, weight: .thin))
-                                                .frame(width: size, height: size)
-                                                .foregroundColor(Color(uiColor: UIColor(white: 0.9, alpha: 1.0)))
-                                        }
-                                        .padding(.top, size * 0.5)
-                                        Text("copy")
-                                            .foregroundColor(.white)
-                                            .padding(.top, size * 0.5)
-                                            .frame(width: size, height: size)
-                                            .minimumScaleFactor(0.01)
-                                        Text("paste")
-                                            .foregroundColor(.white)
-                                            .padding(.top, size * 0.5)
-                                            .frame(width: size, height: size)
-                                            .minimumScaleFactor(0.01)
-                                    }
-                                }
-                                Spacer(minLength: 0.0)
-                            }
-                        }
-                    }
+                    Icons(isPortrait: isPortrait,
+                          height: keyboardSize.height,
+                          isCalculating: model.isCalculating,
+                          isZoomed: model.zoomed,
+                          model: model,
+                          keyInfo: model.keyInfo["plusKey"]!)
                 }
             
                 .overlay() { /// keyboard
-                    let info = "\(model._hasBeenReset ? "Precision: "+model.precisionDescription+" digits" : "\(Model._rad ? "Rad" : "")")"
-                    VStack(spacing: 0.0) {
-                        Spacer()
-                        if !isPortrait {
-                            HStack(spacing: 0.0) {
-                                Text(info).foregroundColor(.white)
-                                    .offset(x: keyHeight * 0.3, y: keyHeight * -0.05)
-                                Spacer()
-                            }
-                        }
-                        KeysView(model: model, isScientific: !isPortrait, size: keyboardSize)
-                    }
-                    .transition(.move(edge: .bottom))
-                    .offset(y: (model.zoomed && !isPortrait) ? size.height : 0)
+                    KeysViewAndText(hasBeenReset: model._hasBeenReset,
+                                    precisionDescription: model.precisionDescription,
+                                    rad: Model._rad,
+                                    isPortrait: isPortrait,
+                                    keyHeight: keyHeight,
+                                    model: model,
+                                    keyboardSize: keyboardSize,
+                                    zoomed: model.zoomed,
+                                    size: size)
                 }
                 .onRotate { newOrientation in /// this magically reduces the number of haveResultCallback() calls to one per rotation
                     orientation = newOrientation
@@ -129,6 +66,131 @@ struct Calculator: View {
     }
 }
 
+struct Display: View {
+    let isPortrait: Bool
+    let isZoomed: Bool
+    let displayData: DisplayData
+    let lengths: Lengths
+    let singleLineFontSize: CGFloat
+    let displayWidth: CGFloat
+    let height: CGFloat
+    let displayTrailingOffset: CGFloat
+    let displayBottomOffset: CGFloat
+    var body: some View {
+        VStack(spacing: 0.0) {
+            if isPortrait {
+                PortraitDisplay(
+                    displayData: displayData,
+                    fullLength: displayData.short.count == lengths.withoutComma,
+                    //ePadding: model.lengths.ePadding,
+                    smallFont: Font(UIFont.monospacedDigitSystemFont(ofSize: singleLineFontSize, weight: .thin)),
+                    largeFont: Font(UIFont.monospacedDigitSystemFont(ofSize: singleLineFontSize * 1.5, weight: .thin)),
+                    fontScaleFactor: 1.5,
+                    displayWidth: displayWidth)
+                //.background(Color.yellow)
+                .padding(.bottom, height)
+                //                        .offset(y: displayYOffset)
+            } else {
+                HStack(spacing: 0.0) {
+                    LandscapeDisplay(
+                        zoomed: isZoomed,
+                        displayData: displayData,
+                        ePadding: lengths.ePadding,
+                        font: Font(UIFont.monospacedDigitSystemFont(ofSize: singleLineFontSize, weight: .thin)),
+                        isCopyingOrPasting: false,
+                        precisionString: "Model.precision.useWords",
+                        displayWidth: displayWidth)
+                    .offset(x: -displayTrailingOffset, y: displayBottomOffset)
+                    //                        .background(Color.green).opacity(0.4)
+                    .animation(Animation.easeInOut(duration: 0.4), value: isZoomed)
+                }
+                //.background(Color.yellow)
+                Spacer()
+            }
+        }
+    }
+}
+
+struct Icons : View {
+    let isPortrait: Bool
+    let height: CGFloat
+    let isCalculating: Bool
+    let isZoomed: Bool
+    let model: Model
+    let keyInfo: Model.KeyInfo
+    var body: some View {
+        if !isPortrait {
+            HStack(spacing: 0.0) {
+                Spacer(minLength: 0.0)
+                let size = height * 0.13
+                VStack(spacing: 0.0) {
+                    //let _ = print("model.isCalculating \(model.isCalculating)")
+                    if isCalculating {
+                        AnimatedDots(color: .gray)
+                            .padding(.top, size * 0.55)
+                            .animation(Animation.easeInOut(duration: 0.4), value: isZoomed)
+                    } else {
+                        PlusKey(keyInfo: keyInfo, model: model, size: CGSize(width: size, height: size))
+                            .padding(.top, size * 0.25)
+                        //                                    .offset(y: size * 0.25)
+                        if model.zoomed {
+                            NavigationLink {
+                                Settings(model: model)
+                            } label: {
+                                Image(systemName: "switch.2")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .font(.system(size: height, weight: .thin))
+                                    .frame(width: size, height: size)
+                                    .foregroundColor(Color(uiColor: UIColor(white: 0.9, alpha: 1.0)))
+                            }
+                            .padding(.top, size * 0.5)
+                            Text("copy")
+                                .foregroundColor(.white)
+                                .padding(.top, size * 0.5)
+                                .frame(width: size, height: size)
+                                .minimumScaleFactor(0.01)
+                            Text("paste")
+                                .foregroundColor(.white)
+                                .padding(.top, size * 0.5)
+                                .frame(width: size, height: size)
+                                .minimumScaleFactor(0.01)
+                        }
+                    }
+                    Spacer(minLength: 0.0)
+                }
+            }
+        }
+    }
+}
+
+struct KeysViewAndText: View {
+    let hasBeenReset: Bool
+    let precisionDescription: String
+    let rad: Bool
+    let isPortrait: Bool
+    let keyHeight: CGFloat
+    let model: Model
+    let keyboardSize: CGSize
+    let zoomed: Bool
+    let size: CGSize
+    var body: some View {
+        let info = "\(hasBeenReset ? "Precision: "+precisionDescription+" digits" : "\(rad ? "Rad" : "")")"
+        VStack(spacing: 0.0) {
+            Spacer()
+            if !isPortrait {
+                HStack(spacing: 0.0) {
+                    Text(info).foregroundColor(.white)
+                        .offset(x: keyHeight * 0.3, y: keyHeight * -0.05)
+                    Spacer()
+                }
+            }
+            KeysView(model: model, isScientific: !isPortrait, size: keyboardSize)
+        }
+        .transition(.move(edge: .bottom))
+        .offset(y: (model.zoomed && !isPortrait) ? size.height : 0)
+    }
+}
 extension View {
     func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
         self.modifier(DeviceRotationViewModifier(action: action))
