5543c9d2e5e94b67c52cf4dbc5ff871b627996f8
Joachim Neumann <joachim@joachimneumann.com>
2023-01-09 17:52:24 +0700

    test in landscape

--- Calculator_UITests/Calculator_UITests.swift
+++ Calculator_UITests/Calculator_UITests.swift
@@ -12,17 +12,22 @@ final class Calculator_UITests: XCTestCase {
     let app = XCUIApplication()
 
     override func setUpWithError() throws {
+        let device = XCUIDevice.shared
+        device.orientation = .landscapeRight
         continueAfterFailure = false
         app.launch()
         print(app.debugDescription)
-        app.staticTexts["KeyID_AC"].tap()
-        let infoText = app.staticTexts["infoText"]
-        XCTAssertEqual(infoText.label, "Precision: ten million digits")
     }
 
     override func tearDownWithError() throws {
     }
 
+    func test_infoText() {
+        app.staticTexts["KeyID_AC"].tap()
+        let infoText = app.staticTexts["infoText"]
+        XCTAssertEqual(infoText.label, "Precision: ten million digits")
+    }
+    
     func test_ln_0_infinity() throws {
         app.staticTexts["KeyID_ln"].tap()
         XCTAssertEqual(app.staticTexts["landscapeDisplayText"].label, "infinity")
@@ -39,6 +44,28 @@ final class Calculator_UITests: XCTestCase {
         app.staticTexts["KeyID_7"].tap()
         app.images["KeyID_±"].tap()
         XCTAssertEqual(app.staticTexts["landscapeDisplayText"].label, "-7")
+    }
+
+    func test_2nd() throws {
+        let app = XCUIApplication()
+        app.staticTexts["KeyID_2^nd"].tap()
+        app.staticTexts["KeyID_2^nd"].tap()
+        app.staticTexts["KeyID_sin"].tap()
+        XCTAssertEqual(app.staticTexts["landscapeDisplayText"].label, "9")
+    }
+
+    func test_9_sin_cos_tan() throws {
+        let app = XCUIApplication()
+        app.staticTexts["KeyID_AC"].tap()
+        app.staticTexts["KeyID_9"].tap()
+        app.staticTexts["KeyID_sin"].tap()
+        app.staticTexts["KeyID_cos"].tap()
+        app.staticTexts["KeyID_tan"].tap()
+        app.staticTexts["KeyID_2^nd"].tap()
+        app.staticTexts["KeyID_tan"].tap()
+        app.staticTexts["KeyID_cos"].tap()
+        app.staticTexts["KeyID_sin"].tap()
+        XCTAssertEqual(app.staticTexts["landscapeDisplayText"].label, "9")
     }
     
 
